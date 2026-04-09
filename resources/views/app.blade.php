788c725b71c668357282c44e59e2a0ec2ebcd186
irwanfelani <sansdigital.id@gmail.com>
2026-04-09 10:43:57 +0700

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,10 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
+        <!-- Favicon -->
+        <link rel="icon" href="/images/logo.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
         <!-- Pi SDK -->
         <script src="https://sdk.minepi.com/pi-sdk.js"></script>
         <script>Pi.init({ version: "2.0", sandbox: {{ config('services.pi.sandbox') ? 'true' : 'false' }} })</script>
