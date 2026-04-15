62e27a8c82abe319e911f87f3f032a79f1c2a8e9
irwanfelani <sansdigital.id@gmail.com>
2026-04-15 17:28:42 +0700

    Update AdminDashboardController.php

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+use App\Models\Order;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
